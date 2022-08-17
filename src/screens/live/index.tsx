9393e7a932f2d324365b895725287262a16cd7a3
moon <mhjnice87@gmail.com>
2022-08-17 16:46:10 +0900

    20220817 - file

--- src/screens/live/index.tsx
+++ src/screens/live/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect} from 'react';
 import { ScrollView } from 'react-native';
 import TopNavigation from 'component/TopNavigation';
 import { styles } from 'assets/styles/Styles';
@@ -6,8 +7,83 @@ import SpaceView from 'component/SpaceView';
 import { CommonText } from 'component/CommonText';
 import { ViualSlider } from 'component/ViualSlider';
 import { RadioCheckBox } from 'component/RadioCheckBox';
+import { jwt_token } from 'utils/properties';
+import { CommonCode, LabelObj, ProfileImg } from '@types';
+import axios from 'axios';
 
 export const Live = () => {
+
+	// 공통코드
+	const [commonCodeList, setCommonCodeList] = useState([CommonCode]);
+
+	// 회원 인상 정보
+	const [profileImg, setProfileImg] = useState([ProfileImg]);
+
+	// 회원 인상 정보
+	const [faceTypeList, setFaceTypeList] = useState([LabelObj]);
+
+	const getProfileImg = async () => {
+		
+		const result = await axios.post('http://192.168.35.131:8080/match/selectProfileImg', {
+			'api-key' : 'U0FNR09CX1RPS0VOXzAx'
+		}
+		, {
+			headers: {
+				'jwt-token' : String(await jwt_token())
+			}
+		})
+		.then(function (response) {
+			if(response.data.result_code != '0000'){
+				return false;
+			}
+
+			setProfileImg(response.data);
+		})
+		.catch(function (error) {
+			console.log('error ::: ' , error);
+		});
+	}
+
+	const getFaceType = async () => {
+		
+		const result = await axios.post('http://192.168.35.131:8080/common/selectGroupCodeList', {
+			'api-key' : 'U0FNR09CX1RPS0VOXzAx'
+			, 'group_code' : 'OPPOSITE_FACE'
+		}
+		, {
+			headers: {
+				'jwt-token' : String(await jwt_token())
+			}
+		})
+		.then(function (response) {
+			if(response.data.result_code != '0000'){
+				return false;
+			}
+
+			const tmpfaceTypeList = [LabelObj];
+
+			setCommonCodeList(response.data.result)
+
+			commonCodeList.map(commonCode => {
+				tmpfaceTypeList.push({label: commonCode.code_name, value:commonCode.common_code})
+			});
+
+			setFaceTypeList(tmpfaceTypeList.filter(Boolean));
+		})
+		.catch(function (error) {
+			console.log('error ::: ' , error);
+		});
+	}
+
+	// 첫 렌더링 때 fetchNews() 한 번 실행
+	useEffect(() => {
+		// 프로필 이미지 목록
+		getProfileImg();
+		
+		// 인상정보
+		getFaceType();
+	}, []);
+	
 	return (
 		<>
 			<TopNavigation currentPath={'LIVE'} />
@@ -22,19 +98,9 @@ export const Live = () => {
 							인상을 선택해주세요.
 						</CommonText>
 					</SpaceView>
-
-					<RadioCheckBox
-						items={[
-							{ label: '눈이 이뻐요', value: "'눈이 이뻐요'" },
-							{ label: '그림 같은 눈썹', value: "'눈이 이뻐요'" },
-							{ label: '봄같은 분위기', value: "'눈이 이뻐요'" },
-							{ label: '섹시한', value: "'눈이 이뻐요'" },
-							{ label: '분위기 여신', value: "'눈이 이뻐요'" },
-							{ label: '이국적인', value: "'눈이 이뻐요'" },
-							{ label: '싱그러운', value: "'눈이 이뻐요'" },
-							{ label: '귀여운 턱선', value: "'눈이 이뻐요'" },
-						]}
-					/>
+					{
+						faceTypeList != null? <RadioCheckBox items={faceTypeList} /> : null
+					}
 				</SpaceView>
 			</ScrollView>
 		</>
