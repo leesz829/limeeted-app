fb4548e9f79658f3aa3b7b6bbcc60fbc9f91d19d
zoon_home <zoon829@naver.com>
2022-09-14 20:05:06 +0900

    20220914 lee

--- src/utils/properties.ts
+++ src/utils/properties.ts
@@ -1,10 +1,27 @@
 import { AsyncStorage } from 'react-native';
 
+// api 서버 도메인
+export const api_domain = "http://211.104.55.151:8080";
+
 // token 값 
 export const jwt_token = () => {
 	return new Promise( async (resolve, reject) => {
 		try {
 			const value = await AsyncStorage.getItem('jwt-token');
+			resolve(value);
+		} catch (error) {
+			reject(new Error('Error getting item from AsyncStorage'))
+		}
+	});
+}
+
+// json 데이터 
+export const get_json_data = (key: string) => {
+	return new Promise( async (resolve, reject) => {
+		try {
+			console.log('key :::: ', key);
+			const value = await AsyncStorage.getItem(key);
+			console.log('value :::: ', value);
 			resolve(value);
 		} catch (error) {
 			reject(new Error('Error getting item from AsyncStorage'))
