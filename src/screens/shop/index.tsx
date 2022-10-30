9a6dd515c68c92dd98e4e2bbf2d2b4402dc0e002
moon <mhjnice87@gmail.com>
2022-10-30 13:29:18 +0900

    마켓 배포버전

--- src/screens/shop/index.tsx
+++ src/screens/shop/index.tsx
@@ -1,13 +1,134 @@
 import * as React from 'react';
-import { Image, ScrollView, View } from 'react-native';
+import { Image, ScrollView, View, Platform, Alert} from 'react-native';
 import TopNavigation from 'component/TopNavigation';
 import { ICON } from 'utils/imageUtils';
 import { layoutStyle, styles } from 'assets/styles/Styles';
 import SpaceView from 'component/SpaceView';
 import { CommonText } from 'component/CommonText';
 import { ColorType } from '@types';
+import * as RNIap from "react-native-iap";
 
 export const Shop = () => {
+
+	let purchaseUpdateSubscription: any;
+	let purchaseErrorSubscription: any;
+	const [loading, setLoading] = React.useState(false);
+
+	// 결제 요청후 리스폰스를 받을 리스너
+	function useShoppingState() {
+		const connection = async () => {
+			try {
+				const init = await RNIap.initConnection();
+				const initCompleted = init === true;
+				
+				if (initCompleted) {
+					if (Platform.OS === 'android') {
+						await RNIap.flushFailedPurchasesCachedAsPendingAndroid();
+					} else {
+						await RNIap.clearTransactionIOS();
+					}
+				}
+				
+				// success listener
+				purchaseUpdateSubscription = RNIap.purchaseUpdatedListener(
+					async (purchase: RNIap.ProductPurchase | RNIap.SubscriptionPurchase) => {
+						const receipt = purchase.transactionReceipt ? purchase.transactionReceipt : purchase.purchaseToken;
+						// type 오류 방지용 변수, 초기값 세팅 필요시 사용
+						const productPurchase:any = null;
+
+						if (receipt) {
+							try {
+								setLoading(false);
+									const ackResult = await RNIap.finishTransaction(purchase?purchase:productPurchase);
+								
+								// 구매이력 저장 및 상태 갱신
+								if (purchase) {
+								
+								}
+							} catch(error) {
+								console.log('ackError: ', error);
+							}
+						}
+					}
+				);
+				
+				purchaseErrorSubscription = RNIap.purchaseErrorListener((error: RNIap.PurchaseError) => {
+					setLoading(false);
+					
+					// 정상적인 에러상황 대응
+					if (error && error.code == RNIap.ErrorCode.E_USER_CANCELLED) {
+						Alert.alert('구매 취소', '구매를 취소하셨습니다.');
+					} else {
+						Alert.alert('구매 실패', '구매 중 오류가 발생하였습니다.');
+					}
+				});
+			} catch(error) {
+				console.log('connection error: ', error);
+			}
+		}
+		
+		connection();
+
+		return () => {
+			if (purchaseUpdateSubscription) {
+				purchaseUpdateSubscription.remove();
+				purchaseUpdateSubscription = null;
+			}
+			
+			if (purchaseErrorSubscription) {
+				purchaseErrorSubscription.remove();
+				purchaseErrorSubscription = null;
+			}
+		
+			RNIap.endConnection();
+		}
+	}
+
+	
+	// 구독상품용 변수
+	const itemSubs: any = Platform.select({
+		ios: [
+			'cash_100'
+		],
+		android: [
+			'cash_100'
+		]
+	});
+
+	// 단일 상품용 변수
+	const itemSkus: any = Platform.select({
+		ios: [
+			'cash_100'
+		],
+		android: [
+			'cash_100'
+		]
+	});
+	
+	const getItems = async () => {
+		try {
+			console.log('1');
+			await RNIap.initConnection()
+			console.log('2');
+    		const items = await RNIap.getProducts(itemSkus)
+			// const items = await RNIap.getProducts(itemSkus);
+			// const items = await RNIap.getProducts(itemSkus);
+			// items 저장
+			Alert.alert('test data .... ' + items);
+			
+		} catch(error) {
+			Alert.alert('test data error .... ' + error);
+			console.log('get item error: ', error);
+		}
+	}
+
+
+	React.useEffect(() => {
+		  console.log(4);
+		  getItems();
+		  console.log(5);
+	}, [])
+
 	return (
 		<>
 			<TopNavigation currentPath={''} />
@@ -79,7 +200,7 @@ export const Shop = () => {
 							<CommonText fontWeight={'500'}>10</CommonText>
 						</SpaceView>
 						<View>
-							<CommonText fontWeight={'700'}>₩9,900</CommonText>
+							<CommonText fontWeight={'700'}>₩100</CommonText>
 						</View>
 					</View>
 					<View style={styles.rowStyle}>
