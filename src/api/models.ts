3fcc1e9d2b67691756f3b0ad0a1cfc6d97584742
박창섭 <snp0315@jobkorea.co.kr>
2022-11-10 16:40:14 +0900

    add test function

--- src/api/models.ts
+++ src/api/models.ts
@@ -3,6 +3,15 @@ import properties from 'utils/properties';
 import { MY_ACCOUNT } from './route';
 
 //### 네트워크 모델 함수이름은 snake_case를 사용해주세요
+
+//========================GET==========================
+export async function some_get_functions() {
+	return Send('any route', 'GET', undefined, true, false);
+}
+//=====================================================
+
+//========================POST=========================
+
 export async function get_my_info() {
 	const member_seq = await properties.get_json_data('member_seq');
 	const body = {
@@ -12,3 +21,17 @@ export async function get_my_info() {
 
 	return Send(MY_ACCOUNT, 'POST', body, true, false);
 }
+
+//=====================================================
+
+//========================PUT=======================
+export async function some_update_functions() {
+	return Send('any route', 'PUT', undefined, true, false);
+}
+//=====================================================
+
+//========================DELETE=======================
+export async function some_delete_functions() {
+	return Send('any route', 'DELETE', undefined, true, false);
+}
+//=====================================================
