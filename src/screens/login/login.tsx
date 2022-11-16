4f5c52e9e1aa0e977670982497dc333c51406afc
박창섭 <snp0315@jobkorea.co.kr>
2022-11-16 22:34:27 +0900

    ios config

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -32,7 +32,7 @@ GoogleSignin.configure({
 	offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
 	forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
 	accountName: '', // [Android] specifies an account name on the device that should be used
-	iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+	iosClientId: '369959009315-44q2i0a8ov52bdbmhb38j906ho577i94.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
 	googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
 	openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
 	profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
