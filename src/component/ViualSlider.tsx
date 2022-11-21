97e9e748affa6ae0dd966090d16823716c4595b6
박창섭 <snp0315@jobkorea.co.kr>
2022-11-22 03:39:29 +0900

    Carousel => FlatList 변경

--- src/component/ViualSlider.tsx
+++ src/component/ViualSlider.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import { View, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, Image, TouchableOpacity, StyleSheet, Dimensions, FlatList } from 'react-native';
 import type { FC } from 'react';
 import SpaceView from './SpaceView';
 import { useState, useEffect } from 'react';
 import { ICON, IMAGE } from 'utils/imageUtils';
 import { CommonText } from './CommonText';
-import { ColorType} from '@types';
+import { ColorType } from '@types';
 import { layoutStyle } from 'assets/styles/Styles';
 import Carousel from 'react-native-reanimated-carousel';
 
 const { width } = Dimensions.get('window');
 interface Props {
-	imgUrls?: {url: string}[];
+	imgUrls?: { url: string }[];
 	profileName?: string;
 	age?: string;
 	status?: string;
@@ -19,7 +19,7 @@ interface Props {
 	isNew?: boolean;
 	onlyImg?: boolean;
 	num?: number;
-	callBackFunction?: (activeType:string) => void;
+	callBackFunction?: (activeType: string) => void;
 }
 
 /**
@@ -29,20 +29,20 @@ interface Props {
  * @param {boolean} onlyImg 이미지와 닉네임 거리만 나타남
  * @returns
  */
-export const ViualSlider : FC<Props> = (props) => {
+export const ViualSlider: FC<Props> = (props: Props) => {
 	const [currentIndex, setCurrentIndex] = React.useState(0);
 
-	const callBackFunction = (activeType:string) => {
+	const callBackFunction = (activeType: string) => {
 		console.log('activeType ::: ', activeType);
 
 		props.callBackFunction && props.callBackFunction(activeType);
-	}
+	};
 
-	// 첫 렌더링 때 fetchNews() 한 번 실행
-	React.useEffect(() => {
-		console.log('props.imgUrls ::: ', props.imgUrls);
-	}, []);
-	
+	const handleScroll = (event) => {
+		let contentOffset = event.nativeEvent.contentOffset;
+		let index = Math.floor(contentOffset.x / 300);
+		setCurrentIndex(index);
+	};
 	return (
 		<SpaceView>
 			<View style={styles.processContainer}>
@@ -58,32 +58,24 @@ export const ViualSlider : FC<Props> = (props) => {
 			</View>
 
 			<View style={styles.pagingContainer}>
-				{
-				props.imgUrls &&
-				props.imgUrls.map((item, index) => (
-					<View style={styles.dotContainerStyle} key={'dot' + index}>
-						<View style={[styles.pagingDotStyle, index === currentIndex && styles.activeDot]} />
-					</View>
-				))}
+				{props.imgUrls &&
+					props.imgUrls.map((item, index) => (
+						<View style={styles.dotContainerStyle} key={'dot' + index}>
+							<View style={[styles.pagingDotStyle, index === currentIndex && styles.activeDot]} />
+						</View>
+					))}
 			</View>
 
 			<View>
-				{null != props.imgUrls ? (
-					<>
-						<Carousel
-							onScrollEnd={(_, current:any) => setCurrentIndex(current)}
-							loop={false}
-							data={props.imgUrls}
-							width={width}
-							height={480}
-							renderItem={(data) => <RenderItem imgUrl={data} />}
-							/* renderItem={({ item }) => {
-								return <View></View>;
-							}} */
-						/>
-					</>
-				) : null}
-				
+				<FlatList
+					onScroll={handleScroll}
+					showsHorizontalScrollIndicator={false}
+					pagingEnabled={true}
+					horizontal={true}
+					data={props.imgUrls}
+					renderItem={(data) => <RenderItem imgUrl={data} />}
+				/>
+
 				<View style={styles.viusalDescContainer}>
 					<SpaceView mb={8} viewStyle={[layoutStyle.row, layoutStyle.alignCenter]}>
 						<CommonText fontWeight={'700'} type={'h3'} color={ColorType.white}>
@@ -114,7 +106,7 @@ export const ViualSlider : FC<Props> = (props) => {
 						<></>
 					) : (
 						<View style={layoutStyle.rowBetween}>
-							<View style={styles.useActionBoxSmall} >
+							<View style={styles.useActionBoxSmall}>
 								<TouchableOpacity onPress={() => callBackFunction('pass')}>
 									<Image source={ICON.close} style={[styles.iconSize32]} />
 								</TouchableOpacity>
@@ -151,22 +143,25 @@ export const ViualSlider : FC<Props> = (props) => {
 	);
 };
 
-const RenderItem = (imgObj:any) => {
-
+const RenderItem = (imgObj: any) => {
 	return (
 		<View>
-			{
-				!imgObj.imgUrl.item.url ?
-				<Image source={{uri : 'http://118.67.134.149:8080/uploads/profile/rn_image_picker_lib_temp_6813179b-95c7-416c-84da-51ab9ca0dbc1.jpg'}} style={styles.visualImage} />
-				: <Image source={{uri : imgObj.imgUrl.item.url}} style={styles.visualImage} />
-			}
-			
+			{!imgObj.imgUrl.item.url ? (
+				<Image
+					source={{
+						uri: 'http://118.67.134.149:8080/uploads/profile/rn_image_picker_lib_temp_6813179b-95c7-416c-84da-51ab9ca0dbc1.jpg',
+					}}
+					style={styles.visualImage}
+				/>
+			) : (
+				<Image source={{ uri: imgObj.imgUrl.item.url }} style={styles.visualImage} />
+			)}
+
 			{/* {
 				!imgObj.imgUrl.item.url ?
 				<Image source={{uri : 'http://118.67.134.149:8080/uploads/profile/rn_image_picker_lib_temp_6813179b-95c7-416c-84da-51ab9ca0dbc1.jpg'}} style={styles.visualImage} />
 				: <Image source={{uri : 'http://118.67.134.149:8080/uploads/tmp/rn_image_picker_lib_temp_27af3da1-87a1-4275-951f-3ff7ad09cec3.jpg'}} style={styles.visualImage} />
 			} */}
-			
 		</View>
 	);
 };
