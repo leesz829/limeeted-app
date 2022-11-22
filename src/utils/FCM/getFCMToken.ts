8e4121726bd831329ec7eb92594b5bda8fb0ceab
박창섭 <snp0315@jobkorea.co.kr>
2022-11-22 21:33:40 +0900

    async-storage repo fix

--- src/utils/FCM/getFCMToken.ts
+++ src/utils/FCM/getFCMToken.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import messaging from '@react-native-firebase/messaging';
 
 export default async function getFCMToken() {
