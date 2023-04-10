4087b935c195884d3ee6d77cc8f00c47a6e7c19e
leesungzoon <zoon9801@gmail.com>
2023-04-11 02:31:22 +0900

    properties

--- src/utils/properties.ts
+++ src/utils/properties.ts
@@ -3,11 +3,11 @@ import { JWT_TOKEN } from 'constants/storeKey';
 
 // api 서버 도메인
 //export const api_domain = "http://59.5.92.64:9915";
-export const api_domain = 'http://118.67.134.149:9915';
-// export const api_domain = 'http://59.5.92.43:8080';
+//export const api_domain = 'http://118.67.134.149:9915';
+export const api_domain = 'http://115.85.182.94';
 
 // 이미지 서버 경로
-export const img_domain = 'http://118.67.134.149:9915' + '/uploads';
+export const img_domain = 'http://115.85.182.94' + '/uploads';
 
 // token 값
 export const jwt_token = () => {
