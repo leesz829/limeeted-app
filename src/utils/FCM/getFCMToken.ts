2e114bb5af8433dda5724c2b655a3bcc9536ca95
moon <mhjnice87@gmail.com>
2023-05-08 23:10:25 +0900

    토큰초기화 확인

--- src/utils/FCM/getFCMToken.ts
+++ src/utils/FCM/getFCMToken.ts
@@ -1,7 +1,26 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import messaging from '@react-native-firebase/messaging';
+import firebase from '@react-native-firebase/app';
+import {Platform} from 'react-native';
 
 export default async function getFCMToken() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZUNI6SD-BBPp_3ep7LRSxUzlgjicSByA",
+    authDomain: "limeeted-ea344.firebaseapp.com",
+    databaseURL: "https://limeeted-ea344.firebaseio.com",
+    projectId: "limeeted-ea344",
+    storageBucket: "limeeted-ea344.appspot.com",
+    messagingSenderId: "535563482959",
+    appId: "1:535563482959:android:d21b373a4e414cdf07b73a",
+  };
+
+  if (Platform.OS === 'android') {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }
+  
   const storedFcmToken = await AsyncStorage.getItem('fcmToken');
   await messaging().registerDeviceForRemoteMessages();
   const FCMToken = storedFcmToken || (await messaging().getToken());
