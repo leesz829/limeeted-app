8baaffaa4644a7b56169a3b5c5b4536be4d3eaed
leesungzoon <zoon9801@gmail.com>
2023-07-27 18:15:11 +0900

    [1.0.1 빌드] 하단 네비게이션 리밋샵 노출 말풍선 추가

--- src/component/TabIcon.tsx
+++ src/component/TabIcon.tsx
@@ -1,11 +1,38 @@
-import { View, Image, StyleSheet, Text } from 'react-native';
+import { View, Image, StyleSheet, Text, Animated } from 'react-native';
 import { findSourcePath, ICON } from 'utils/imageUtils';
 import * as React from 'react';
 import { useProfileImg } from 'hooks/useProfileImg';
 import { useUserInfo } from 'hooks/useUserInfo';
+import { CommaFormat } from 'utils/functions';
+
 
 const TabIcon = ({ name, isFocused }: { name: string; isFocused: boolean }) => {
   const memberBase = useUserInfo();
+
+  const fadeAnim = new Animated.Value(1);
+
+  const fadeInOut = () => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000, // 깜빡임이 서서히 사라지는 데 걸리는 시간 (1초)
+        delay: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000, // 깜빡임이 서서히 나타나는 데 걸리는 시간 (1초)
+        delay: 2000,
+        useNativeDriver: true,
+      }),
+    ]).start(() => fadeInOut());
+  };
+
+  /* React.useEffect(() => {
+    fadeInOut();
+  }, []); */
+
+  fadeInOut();
 
   switch (name) {
     case 'Roby': {
@@ -51,9 +78,22 @@ const TabIcon = ({ name, isFocused }: { name: string; isFocused: boolean }) => {
           {isFocused ? <Image style={_style.iconSize} source={ICON.cashshopOn} /> :
             <Image style={_style.iconSize} source={ICON.cashshop} />
           }
-          {memberBase?.new_item_cnt != null && typeof memberBase?.new_item_cnt != 'undefined' && memberBase?.new_item_cnt > 0 &&
+          {(memberBase.gender == 'M' && memberBase?.new_item_cnt != null && typeof memberBase?.new_item_cnt != 'undefined' && memberBase?.new_item_cnt > 0) &&
             <View style={_style.shopIconArea}><Text style={_style.newText}>NEW</Text></View>
-          } 
+          }
+          {memberBase.gender == 'W' && (
+            <>
+              {/* <View style={_style.shopLimitArea}>
+                <Text style={_style.limitText}>{CommaFormat(memberBase.mileage_point)}리밋 보유 중!{'\n'}리밋샵에 획득 가능한 상품이 있어요</Text>
+                <View style={_style.triangle}></View>
+              </View> */}
+
+              <Animated.View style={[_style.shopLimitArea, { opacity: fadeAnim }]}>
+                <Text style={_style.limitText}>{CommaFormat(memberBase.mileage_point)}리밋 보유 중!{'\n'}리밋샵에 획득 가능한 상품이 있어요</Text>
+                <View style={_style.triangle}></View>
+              </Animated.View>
+            </>
+          )}
         </>
       )
     }
@@ -82,7 +122,7 @@ const _style = StyleSheet.create({
   },
   iconArea: {
     position: 'absolute',
-    top: -7,
+    top: -3,
     right: 25,
   },
   countText: {
@@ -101,6 +141,12 @@ const _style = StyleSheet.create({
     top: -7,
     right: 16,
   },
+  shopLimitArea: {
+    position: 'absolute',
+    top: -33,
+    right: 7,
+    alignItems: 'flex-end',
+  },
   newText: {
     backgroundColor: '#FF7E8C',
     fontFamily: 'AppleSDGothicNeoEB00',
@@ -110,5 +156,29 @@ const _style = StyleSheet.create({
     paddingHorizontal: 6,
     paddingVertical: 1,
     overflow: 'hidden',
-  }
+  },
+  limitText: {
+    fontFamily: 'AppleSDGothicNeoM00',
+    fontSize: 8,
+    color: '#FFF',
+    backgroundColor: '#7F67FF',
+    borderRadius: 3,
+    textAlign: 'center',
+    width: 120,
+    paddingVertical: 2,
+  },
+  triangle: {
+    marginTop: -1,
+    marginRight: 36,
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderLeftWidth: 6,
+    borderRightWidth: 6,
+    borderBottomWidth: 6,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#7F67FF',
+    transform: [{ rotate: '180deg' }],
+  },
 });
