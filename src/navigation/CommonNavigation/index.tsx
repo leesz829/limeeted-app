7a659f75ebe6516d24ddfb8370cc238711519132
leesungzoon <zoon9801@gmail.com>
2023-07-27 18:04:46 +0900

    [1.0.1 빌드] 최근소식 신규 게시글 구분 추가

--- src/navigation/CommonNavigation/index.tsx
+++ src/navigation/CommonNavigation/index.tsx
@@ -9,7 +9,6 @@ import BottomNavigation from '../TabNavigation';
 import Component from 'component';
 import { CommonPopup } from 'screens/commonpopup';
 import { Board0 } from 'screens/commonpopup/Board0';
-import { Board1 } from 'screens/commonpopup/Board1';
 import { Preference } from 'screens/commonpopup/Preference';
 import { Introduce } from 'screens/profile/Introduce';
 import { Profile } from 'screens/profile/Profile';
@@ -52,7 +51,6 @@ const CommonNavigation = () => {
       <CommonStack.Screen name="LivePopup" component={LivePopup} />
       <CommonStack.Screen name="Introduce" component={Introduce} />
       <CommonStack.Screen name="Board0" component={Board0} />
-      <CommonStack.Screen name="Board1" component={Board1} />
       <CommonStack.Screen name="Preference" component={Preference} />
       <CommonStack.Screen name="Profile" component={Profile} />
       <CommonStack.Screen name="Profile1" component={Profile1} />
--- src/screens/commonpopup/Board0.tsx
+++ src/screens/commonpopup/Board0.tsx
@@ -1,18 +1,18 @@
 import { styles, layoutStyle } from 'assets/styles/Styles';
 import CommonHeader from 'component/CommonHeader';
 import { useState } from 'react';
-import { EventRow } from 'component/EventRow';
 import * as React from 'react';
 import { ScrollView, View, Image, Modal, TouchableOpacity, Alert, Text, StyleSheet, Dimensions } from 'react-native';
-import axios from 'axios';
-import * as properties from 'utils/properties';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { ColorType, ScreenNavigationProp, StackParamList } from '@types';
-import { RouteProp, useNavigation } from '@react-navigation/native';
+import { RouteProp, useNavigation, useIsFocused } from '@react-navigation/native';
 import SpaceView from 'component/SpaceView';
 import { CommonText } from 'component/CommonText';
 import { ICON } from 'utils/imageUtils';
 import { Color } from 'assets/styles/Color';
+import { get_board_list, board_detail_view } from 'api/models';
+import { usePopup } from 'Context';
+import { CommonLoading } from 'component/CommonLoading';
 
 
 /* ################################################################################################################
@@ -27,17 +27,91 @@ interface Props {
 }
 
 export const Board0 = (props: Props) => {
-	const [noticeList, setnoticeList] = React.useState<any>(props.route.params.boardList);
+
+	const { show } = usePopup();
+	const [isLoading, setIsLoading] = useState(false);
+	const isFocus = useIsFocused();
+
+	const [noticeList, setnoticeList] = React.useState([]);
 
 	const [activeIndex, setActiveIndex] = useState(-1);
 
-	const toggleAccordion = (index) => {
-		setActiveIndex(activeIndex === index ? -1 : index);
+	// 토글
+	const toggleAccordion = (item:any) => {
+		const board_seq = item.board_seq;
+		const view_yn = item.view_yn;
+
+		if(activeIndex !== board_seq && view_yn == 'N') {
+			boardDetailView(board_seq);
+		};
+
+		setActiveIndex(activeIndex === board_seq ? -1 : board_seq);
 	};
+
+	// 게시글 목록 조회
+	const getBoardList = async () => {
+		setIsLoading(true);
+		try {
+			const { success, data } = await get_board_list();
+			if (success) {
+				if (data.result_code == '0000') {
+					setnoticeList(data.boardList);
+				};
+			} else {
+				show({ content: '오류입니다. 관리자에게 문의해주세요.' });
+			}
+		} catch (error) {
+			show({ content: '오류입니다. 관리자에게 문의해주세요.' });
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	// 게시글 상세 조회
+	const boardDetailView = async (board_seq:any) => {
+		try {
+			const body = {
+				board_seq: board_seq
+			};
+			const { success, data } = await board_detail_view(body);
+			if (success) {
+				if (data.result_code == '0000') {
+					
+					const newList = noticeList.map((item:any, index) => {
+						if(item.board_seq == board_seq) {
+							item.view_yn = 'Y';
+						};
+
+						return item;
+					});
+
+					setnoticeList(newList);
+				};
+			} else {
+				show({ content: '오류입니다. 관리자에게 문의해주세요.' });
+			}
+		} catch (error) {
+			show({ content: '오류입니다. 관리자에게 문의해주세요.' });
+			console.log(error);
+		} finally {
+
+		}
+	};
+
+	// ######################################################################################## 초기 실행 함수
+	React.useEffect(() => {
+		if(isFocus) {
+			getBoardList();
+		};
+	  }, [isFocus]);
 
 	return (
 		<>
+			{isLoading && <CommonLoading />}
+
 			<CommonHeader title={'최근 소식'} />
+			
 			<ScrollView contentContainerStyle={styles.scrollContainer} style={{backgroundColor: '#fff'}}>
 				<SpaceView mb={45}>
 					<View style={layoutStyle.alignStart}>
@@ -45,49 +119,51 @@ export const Board0 = (props: Props) => {
 					</View>
 				</SpaceView>
 
-				{noticeList.map(
-					({
-						board_seq,
-						board_type_name,
-						title,
-						content,
-						reg_dt,
-					}: {
-						board_seq: any;
-						board_type_name: string;
-						title: string;
-						content: string;
-						reg_dt: string;
-					}) => (
-						
-						<View key={board_seq} style={{marginBottom: 10}}>
-							<View style={_styles.rowContainer}>
-								<TouchableOpacity
-									style={_styles.inner}
-									onPress={() => { 
-										toggleAccordion(board_seq);
-									}}
-									activeOpacity={0.3} >
-									
-									<View style={[_styles.titleContainer, activeIndex === board_seq && _styles.active]}>
-										<CommonText textStyle={_styles.titleText} fontWeight={'500'} type={'h5'}>{title}</CommonText>
-									</View>
+				{noticeList.map((item, index) => (
+					<SpaceView mb={10} key={item.board_seq}>
+						<View style={_styles.rowContainer}>
+							<TouchableOpacity
+								style={_styles.inner}
+								activeOpacity={0.3}
+								onPress={() => { 
+									toggleAccordion(item);
+								}}>
+								
+								<View style={[_styles.titleContainer, activeIndex === item.board_seq && _styles.active]}>
+									{/* <View style={{flexDirection:'row'}}>
+										{item.new_yn == 'Y' && (
+											<Text style={_styles.iconType('#FF67F0')}>NEW</Text>
+										)}
+									</View> */}
 
-									<View style={[_styles.iconContainer, activeIndex === board_seq && _styles.activeIcon]}>
-										<Image source={ICON.arrBottom} style={_styles.iconStyle} />
+									<View>
+										{(item.new_yn == 'Y' && item.view_yn == 'N') && (
+											<View style={_styles.newIcon} />
+										)}
+										<CommonText textStyle={_styles.titleText} fontWeight={'500'} type={'h5'}>{item.title}</CommonText>
 									</View>
-								</TouchableOpacity>
-							</View>
-
-							{activeIndex === board_seq && (
-								<View style={_styles.descContainer}>
-									<CommonText textStyle={_styles.descText} type={'h5'}>{content}</CommonText>
-									<CommonText textStyle={_styles.dateText} type={'h5'}>{reg_dt}</CommonText>
 								</View>
-							)}
+
+								<View style={[_styles.iconContainer, activeIndex === item.board_seq && _styles.activeIcon]}>
+									<Image source={ICON.arrBottom} style={_styles.iconStyle} />
+								</View>
+							</TouchableOpacity>
 						</View>
-					),
-				)}
+
+						{/* {new_yn == 'Y' &&
+							<View style={_styles.newArea}>
+								<Text style={_styles.newText}>NEW</Text>
+							</View>
+						} */}
+
+						{activeIndex === item.board_seq && (
+							<View style={_styles.descContainer}>
+								<CommonText textStyle={_styles.descText} type={'h5'}>{item.content}</CommonText>
+								<CommonText textStyle={_styles.dateText} type={'h5'}>{item.reg_dt}</CommonText>
+							</View>
+						)}
+					</SpaceView>
+				))}
 			</ScrollView>
 		</>
 	);
@@ -153,6 +229,40 @@ const _styles = StyleSheet.create({
 		textAlign: 'right',
 		paddingHorizontal: 15,
 		marginTop: 5,
-	}
+	},
+	newArea: {
+		position: 'absolute',
+		top: -10,
+		left: 0,
+		backgroundColor: '#000',
+		borderRadius: 20,
+		paddingHorizontal: 8,
+		paddingVertical: 2,
+	},
+	newText: {
+		fontFamily: 'AppleSDGothicNeoM00',
+		fontSize: 12,
+		color: '#fff',
+	},
+	iconType: (color:string) => {
+		return {
+			width: 50,
+			fontFamily: 'AppleSDGothicNeoEB00',
+			fontSize: 10,
+			color: '#fff',
+			backgroundColor: color,
+			textAlign: 'center',
+			borderRadius: 10,
+			marginRight: 5,
+		};
+	},
+	newIcon: {
+		position: 'absolute',
+		top: 5,
+		left: -7,
+		width: 5,
+		height: 5,
+		backgroundColor: '#FF67F0',
+		borderRadius: 30,
+	},
   });
-  
