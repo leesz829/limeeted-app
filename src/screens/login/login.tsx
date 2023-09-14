49546466226ef928d5990ab8db0bb0f2344b69c0
leesungzoon <zoon9801@gmail.com>
2023-09-14 16:01:05 +0900

    [1.0.6 빌드] SDK33 알림 권한 적용

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -365,7 +365,7 @@ export const Login01 = () => {
         );
 
         if(Platform.OS == 'android') {
-          //requestNotificationPermission();
+          requestNotificationPermission();
         };
       });
     };
